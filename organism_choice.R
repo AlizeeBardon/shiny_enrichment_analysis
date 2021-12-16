aa55ee8a5d752506e8214565ccb09e9fbb5e8099
benchsar <60518421+benchsar@users.noreply.github.com>
2021-12-16 16:04:10 +0100

    Update organism_choice.R

--- organism_choice.R
+++ organism_choice.R
@@ -1,7 +1,6 @@
 # OrgDb ##############################################################
 org_to_db <- function(organism) {
   
-  results <- list()
   organismsDb <-c("Human (org.Hs.eg.db)"="org.Hs.eg.db","Mouse (org.Mm.eg.db)"="org.Mm.eg.db","Rat (org.Rn.eg.db)"="org.Rn.eg.db",
                          "Yeast (org.Sc.sgd.db)"="org.Sc.sgd.db","Fly (org.Dm.eg.db)"="org.Dm.eg.db","Arabidopsis (org.At.tair.db)"="org.At.tair.db",
                          "Zebrafish (org.Dr.eg.db)"="org.Dr.eg.db","Bovine (org.Bt.eg.db)"="org.Bt.eg.db","Worm (org.Ce.eg.db)"="org.Ce.eg.db",
